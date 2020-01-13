75f2e818e421569933ff7e47daaffc40bc7e2f28
Grégory Artaud <gregory.rtd@gmail.com>
2020-01-13 16:24:05 +0100

    mise en place morpion

--- morpion/morpion.c
+++ morpion/morpion.c
@@ -1,1 +1,30 @@
 #include "morpion.h"
+
+const int tailleGrille = 3;
+
+void initMorpion() {
+	for (int i = 0; i < tailleGrille; i++) {
+		for (int j = 0; j < tailleGrille; j++) {
+			morpion[i][j] = VIDE;
+		}
+	}
+}
+
+int jouer(morpion m, int i, int j, int joueur) {
+	if (m[i][j] != VIDE) {
+		return 1;
+	} else {
+		m[i][j] = joueur;
+		return 0;
+	}
+}
+
+int gagnant(morpion m, int joueur) {
+	for (int i = 0; i < tailleGrille; i++) {
+		for (int j = 0; j < tailleGrille; j++) {
+			if (m[i][j] == joueur) {
+				// verifie si m[i][j] est aligné avec n-1 autres
+			}
+		}
+	}
+}
--- morpion/morpion.h
+++ morpion/morpion.h
@@ -1,4 +1,22 @@
 #ifndef _MORPION_H_
 #define _MORPION_H_
 
+#define VIDE -1
+#define JOUEUR1 0
+#define JOUEUR2 1
+
+const int tailleGrille;
+typedef char morpion[tailleGrille][tailleGrille];
+
+// initialise morpion avec la valeur VIDE
+void initMorpion();
+
+// renvoie 1 si m[i][j] est non vide et 0 sinon
+int jouer(morpion m, int i, int j, int joueur);
+
+// renvoie 1 si joueur a gagné
+// si m est plein et que personne n'a gagné renvoie -1
+// renvoie 0 sinon
+int gagnant(morpion m, int joueur);
+
 #endif
