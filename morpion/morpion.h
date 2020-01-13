2efb9eb90e5be466947909f353aff5b87ef7d5ae
Grégory Artaud <gregory.rtd@gmail.com>
2020-01-13 19:30:27 +0100

    fini module morpion

--- morpion/morpion.h
+++ morpion/morpion.h
@@ -5,14 +5,22 @@
 #define JOUEUR1 0
 #define JOUEUR2 1
 
-const int tailleGrille;
-typedef char morpion[tailleGrille][tailleGrille];
+typedef struct {
+	int taille;
+	char ** grille;
+} morpion;
 
-// initialise morpion avec la valeur VIDE
-void initMorpion();
+
+// initialise m avec la valeur VIDE
+void initMorpion(morpion * m, int taille);
 
 // renvoie 1 si m[i][j] est non vide et 0 sinon
-int jouer(morpion m, int i, int j, int joueur);
+int jouer(morpion * m, int i, int j, int joueur);
+
+// renvoie 0 si c'est pas aligné 1 sinon
+int aligneLigne(morpion * m, int ligne, int joueur);
+int aligneColonne(morpion * m, int colonne, int joueur);
+int aligneDiag(morpion * m, int joueur);
 
 // renvoie 1 si joueur a gagné
 // si m est plein et que personne n'a gagné renvoie -1
