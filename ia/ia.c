84658dabd58373e6df0a42893e1b194276c503f9
FONTES Damien <f17019047@l-024110a018-10.salsa.univ-amu.fr>
2020-01-14 14:30:06 +0100

    Module IA

--- ia/ia.c
+++ ia/ia.c
@@ -1,1 +1,77 @@
+#include "morpion.h"
 #include "ia.h"
+#include <limits.h>
+
+int max (morpion *M) {
+  int score, valeur;
+
+  if (gagnant(M, 0)) return 10;
+  else if (gagnant(M, 1)) return -10;
+  else if (gagnant(M, 1) == -1) return 0;
+
+  score -= INT_MAX;
+
+  for (int i=0; i < M->taille; i++) {
+    for (int j=0; j< M->taille; j++) {
+      if (M->grille[i][j] == VIDE) {
+        M->grille[i][j] = JOUEUR1;
+        valeur = min(M);
+
+        if (score < valeur)
+          score = valeur;
+        M->grille[i][j] = VIDE;
+      }
+    }
+  }
+  return score;
+}
+
+int min (morpion *M) {
+  int score, valeur;
+
+  if (gagnant(M, 0)) return 10;
+  else if (gagnant(M, 1)) return -10;
+  else if (gagnant(M, 1) == -1) return 0;
+
+  score += INT_MAX;
+
+  for (int i=0; i < M->taille; i++) {
+    for (int j=0; j< M->taille; j++) {
+      if (M->grille[i][j] == VIDE) {
+        M->grille[i][j] = JOUEUR2;
+        valeur = max(M);
+
+        if (score > valeur)
+          score = valeur;
+        M->grille[i][j] = VIDE;
+      }
+    }
+  }
+  return score;
+}
+
+coordonnees minmax (morpion *M) {
+  int score, valeur;
+  coordonnees C;
+  C.i = 0;
+  C.j = 0;
+
+  score += INT_MAX;
+ for (int i=0; i < M->taille; i++) {
+   for (int j=0; j< M->taille; j++) {
+     if (M->grille[i][j] == VIDE) {
+       M->grille[i][j] = JOUEUR2;
+       valeur = max(M);
+
+       if (score > valeur) {
+         score = valeur;
+         C.i = i;
+         C.j = j;
+       }
+       M->grille[i][j] = VIDE;
+     }
+   }
+ }
+
+ return C;
+}
--- ia/ia.h
+++ ia/ia.h
@@ -1,4 +1,12 @@
 #ifndef _IA_H_
 #define _IA_H_
 
+typedef struct {
+    int i;
+    int j;
+}coordonnees;
+
+int max (morpion *M);
+int min (morpion *M);
+coordonnees minmax (morpion *M);
 #endif
